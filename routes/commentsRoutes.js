b98b1870dd87076a7627d92a82d2bb09a73ff743
DovydasN <klimpau741@gmail.com>
2024-12-02 22:21:11 +0200

    commets done

---
+++ routes/commentsRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const commentsServices = require('../services/commentsServices')
+const router = express.Router()
+
+router.post('/pets/:id/comments', async (req, res) => {
+    const { id } = req.params
+    const { comment, volunteerId } = req.body
+
+    try {
+        const resData = await commentsServices.addComment(id, comment, volunteerId)
+        return res.send(resData)
+    } catch (error) {
+        return res.status(500).send({ error: error.message })
+    }
+})
+
+router.get('/pets/:id/comments', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const comments = await commentsServices.getComments(id)
+        return res.send(comments)
+    } catch (error) {
+        return res.status(500).send({ error: error.message })
+    }
+})
+
+module.exports = router
